29947ec2db47067ed2a6c2f7ddc21c5b2efa7a7f
Katiix <katuha1995@gmail.com>
2022-12-26 20:06:50 +0200

    xpath selectors

--- QA36_Start/src/test/java/Start.java
+++ QA36_Start/src/test/java/Start.java
@@ -36,18 +36,41 @@ public class Start {
 
 
     WebElement log1 = wd.findElement(By.cssSelector("[href='/login']"));
+    WebElement log_1 = wd.findElement(By.xpath("//*[@href='/login']"));
+
     WebElement log2 = wd.findElement(By.cssSelector("#user"));
+    WebElement log_2 = wd.findElement(By.xpath("//*[@id='user']"));
     WebElement log3 = wd.findElement(By.cssSelector("#login"));
+    WebElement log_3 = wd.findElement(By.xpath("//*[@id='login']"));
     WebElement log4 = wd.findElement(By.cssSelector("#already-have-an-account"));
+    WebElement log_4 = wd.findElement(By.xpath("//*[@id='already-have-an-account']"));
 
     WebElement log5 = wd.findElement(By.cssSelector("#password"));
+    WebElement log_5 = wd.findElement(By.xpath("//*[@id='password']"));
     WebElement log6 = wd.findElement(By.cssSelector("#login-submit"));
+    WebElement log_6 = wd.findElement(By.xpath("//*[@id='login-submit']"));
+    WebElement log7 = wd.findElement(By.cssSelector(".m2N684FcksCyfT"));
+    WebElement log_7 = wd.findElement(By.xpath("//*[@class='m2N684FcksCyfT']"));
 
-    
+
+
     WebElement log11 = wd.findElement(By.cssSelector("[aria-label='Trello']"));
+    WebElement log_11 = wd.findElement(By.xpath("//*[@aria-label='Trello']"));
     WebElement log121 = wd.findElement(By.cssSelector("[aria-label $='llo']"));//ends
+
     WebElement log31 = wd.findElement(By.cssSelector("[aria-label ^='Tr']"));//starts
+    WebElement log_31 = wd.findElement(By.xpath("//*[starts-with(@aria-label,'Tr')]"));//starts
     WebElement log41 = wd.findElement(By.cssSelector("[aria-label *='rell']"));//contains
+    WebElement log_121 = wd.findElement(By.xpath("//*[contains(@aria-label,'rell')]"));//contains
+
+
+    //xpath
+    WebElement el1x = wd.findElement(By.xpath("//*[@id='user']"));
+    WebElement el2x = wd.findElement(By.xpath("//*[@class='username-readonly']"));
+    WebElement el3x = wd.findElement(By.xpath("//*[@data-testid='header-member-menu-button']"));
+
+    WebElement ellogc1= wd.findElement(By.cssSelector("[data-testid='header-member-menu-logout']"));
+    WebElement ellogx1= wd.findElement(By.xpath("//*[@data-testid='header-member-menu-logout']"));
 
 
 }
